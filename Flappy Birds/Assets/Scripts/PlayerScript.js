5fd9f8f376c136995246d48e391858e384db859b
supermiu <jeffersonlee2000@ygmail.com>
2014-03-29 16:19:40 -0700

    Don't spam space bar
    At least it moves....

---
+++ Flappy Birds/Assets/Scripts/PlayerScript.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+var speed : float;
+function Start () {
+
+}
+
+function Update () {
+	if(Input.GetKeyDown(KeyCode.Space)) {
+		rigidbody2D.AddForce(Vector2(0,1) * speed);
+	}
+}
--- Flappy Birds/Assets/Scripts/ScrollScript.js
+++ Flappy Birds/Assets/Scripts/ScrollScript.js
@@ -1,4 +1,5 @@
 ﻿#pragma strict
+var seperation : float;
 var back : GameObject;
 var created = false;
 function Start () {
@@ -8,7 +9,7 @@ function Start () {
 function Update () {
 	transform.position.x -= 0.05;
 	if((transform.position.x < -1) && (created == false)) {
-		Instantiate(back,Vector3(19.5,0,0),Quaternion.identity);
+		Instantiate(back,Vector3(seperation,0,0),Quaternion.identity);
 		created = true;
 	}
 	if(transform.position.x < -20) {
