d332ef8e48a4d21cb28f7099054419addb3794fb
supermiu <jeffersonlee2000@ygmail.com>
2014-03-29 10:31:56 -0700

    More Stuff
    PICTUREZ AND STUFF

---
+++ Flappy Birds/Assets/Scripts/ScrollScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+var back : GameObject;
+var created = false;
+function Start () {
+
+}
+
+function Update () {
+	transform.position.x -= 0.05;
+	if((transform.position.x < -1) && (created == false)) {
+		Instantiate(back,Vector3(19,0,0),Quaternion.identity);
+		created = true;
+	}
+	if(transform.position.x < -20) {
+		Destroy(gameObject);
+	} 
+}
