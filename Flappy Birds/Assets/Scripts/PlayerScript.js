5b1672b575e2ed8413f829acfa5464f08a33cc20
supermiu <jeffersonlee2000@ygmail.com>
2014-03-30 20:27:57 -0700

    Beta!
    I'm Done!!!!!
    gonna work on polishing it, but it kinda works.
    AND YOU CAN FINALLY LOSE!

--- Flappy Birds/Assets/Scripts/PlayerScript.js
+++ Flappy Birds/Assets/Scripts/PlayerScript.js
@@ -2,12 +2,17 @@
 var speed : float;
 var tower : GameObject;
 var freebie : GameObject;
+var start : float;
 function Start () {
 	Instantiate(freebie,Vector3(20,0,-1),Quaternion.identity);
+	start = transform.position.x;
 }
 
 function Update () {
 	if(Input.GetKeyDown(KeyCode.Space)) {
 		rigidbody2D.AddForce(Vector2(0,1) * speed);
+	}
+	if(transform.position.x < start) {
+		Application.LoadLevel("main");
 	}
 }
--- Flappy Birds/Assets/Scripts/Tower.js
+++ Flappy Birds/Assets/Scripts/Tower.js
@@ -2,6 +2,7 @@
 var height : float;
 var min : float;
 var max : float;
+var istower = true;
 function Start () {
 	height = Mathf.RoundToInt(Random.Range(min,max));
 	transform.localScale = Vector2(1,height);
