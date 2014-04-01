dad30a58939125744754eed464ef15c051ca8a0f
supermiu <jeffersonlee2000@ygmail.com>
2014-03-31 21:03:37 -0700

    NEW GAME
    workin on a platformer guys.. IN 3D!

---
+++ Platformer/Assets/Scripts/Player.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+var speed : float;
+var jumpheight : float;
+function Start () {
+
+}
+
+function Update () {
+	transform.position += Vector3(Input.GetAxis("Horizontal") / speed,0,Input.GetAxis("Vertical") / speed);
+	if(Input.GetKeyDown(KeyCode.Space)) {
+		if(transform.position.y < 10) {
+			transform.position.y += jumpheight;
+		}
+	}
+	transform.ro
+}
